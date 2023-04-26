1f829b737d31da354a6e4cc70659a751ffe889c7
Shashwat176 <57011731+Shashwat176@users.noreply.github.com>
2023-04-26 15:03:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
-let pages = [];
-
-// Function to create a new page
-function createPage(title, content) {
-    let page = {
-        id: Date.now().toString(),
-        title: title,
-        content: content,
-        url: ""
-    };
-    pages.push(page);
-    return page;
-}
-
-// Function to display pages in the index
-function displayPageIndex() {
-    let pageIndex = document.getElementById("pageIndex");
-    pageIndex.innerHTML = "";
-    pages.forEach(function(page) {
-        let li = document.createElement("li");
-        let a = document.createElement("a");
-        a.textContent = page.title;
-        a.href = page.url;
-        li.appendChild(a);
-        pageIndex.appendChild(li);
+function hashPassword(password) {
+  return crypto.subtle.digest("SHA-256", new TextEncoder().encode(password))
+    .then(hash => {
+      return Array.from(new Uint8Array(hash))
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
     });
 }
 
-// Function to update page URL
-function updatePageUrl(page) {
-    page.url = "page.html?pageId=" + page.id;
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  hashPassword(password).then(hashedPassword => {
+    const credentials = {username: username, password: hashedPassword};
+    localStorage.setItem("credentials", JSON.stringify(credentials));
+    window.location.href = "protected.html";
+  });
 }
 
-// Add event listener for form submit
-document.getElementById("pageForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    let pageTitle = document.getElementById("pageTitle").value;
-    let pageContent = document.getElementById("pageContent").value;
-    let page = createPage(pageTitle, pageContent);
-    updatePageUrl(page);
-    displayPageIndex();
-    document.getElementById("pageTitle").value = "";
-    document.getElementById("pageContent").value = "";
-});
+document.getElementById("login-form").addEventListener("submit", handleSubmit);
