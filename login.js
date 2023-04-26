21042d9c76e87ea7d823b3a67a31f431d9df9a08
Shashwat176 <57011731+Shashwat176@users.noreply.github.com>
2023-04-26 15:49:20 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username === "user" && password === "817495") {
+    if (username === "user" && password === ${{ secrets.ACCESS_TOKEN }}) {
         window.location.href = "protected.html";
     } else {
         document.getElementById("message").innerHTML = "Invalid username or password.";
